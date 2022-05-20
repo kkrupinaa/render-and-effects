9cbc1794235faaff638cf92afb1b473741ccdf4b
kkrupinaa <79110379+kkrupinaa@users.noreply.github.com>
2022-05-20 11:22:05 +0300

    solve

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+    const [message, setMessage] = useState(-1);
+    useEffect(() => {
+        function mycall(message: number) {
+            setMessage(message);
+        }
+        setMessage(-1);
+        subscribe(props.sourceId, mycall);
+        return () => {
+            unsubscribe(props.sourceId, mycall);
+        };
+    }, [props.sourceId]);
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
